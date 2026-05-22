24b0f67d925301e55b0af58a5c0ddc2151d1ad5d
rutujnagargoje2802-afk <rutujnagargoje2802@gmail.com>
2026-05-22 19:42:10 +0530

    Update wsgi.py

--- nachiketa28/wsgi.py
+++ nachiketa28/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Nachiketa28.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nachiketa28.settings')
 
 application = get_wsgi_application()
