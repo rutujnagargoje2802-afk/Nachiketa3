e5de90974fd92b2bb4f3a0689ffdb90927a30b83
rutujnagargoje2802-afk <rutujnagargoje2802@gmail.com>
2026-05-22 21:59:54 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Nachiketa28.settings')
+    # Crucial: Enforce lowercase 'nachiketa28' here
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nachiketa28.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
